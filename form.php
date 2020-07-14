5971ba270d797f967835aa4fede4bb4dea4eff79
ntoine08 <j.antoine689@laposte.net>
2020-07-14 15:08:29 +0200

    6eme video

--- form.php
+++ form.php
@@ -1,27 +1,55 @@
 <?php 
+/**
+ * Class Form
+ * permet de générer un formulaire rapidement et simplement
+ */
 
 class Form{
-
+    /**
+     * @var array données utilisés par le formulaire
+     */
     private $data;
+
+    /**
+     * @var string tag utilisé pour entourer les champs
+     */
     public $surround = 'p';
 
+    /**
+     * @param array $data données utilisés par le formulaire
+     */
     public function __construct($data = array()){
         $this->data = $data;
     }
 
+    /**
+     * @param $html string code html à entourer
+     * @return string
+     */
     private function surround($html){
         return "<{$this->surround}>{$html}</'$this->surround}>";
     }
 
+    /**
+     * @param $index string Index de la valeur à récupérer
+     * @return string
+     */
     private function getValue($index){
         return isset($this->data[$index]) ? $this->data[$index] : null;
     }
 
+    /**
+     * @param $name string 
+     * @return string
+     */
     public function input($name){
         return $this->surround(
              '<input type="text" name="' . $name . '" value="' .$this->getValue($name) . '">');
     }
 
+    /**
+     * @return string
+     */
     public function submit(){
         return $this->surround ('<button type="submit">Envoyer</button>');
     }
